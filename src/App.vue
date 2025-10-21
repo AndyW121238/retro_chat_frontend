d0358dbba6a0664568de1633da71827d47dfc77d
AndyW121238 <626614039@qq.com>
2025-10-21 22:32:14 +0800

    init project

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+
+<template>
+  <router-view class="h-screen w-screen"></router-view>
+</template>
+
+<style scoped></style>
---
+++ vite.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import tailwindcss from "@tailwindcss/vite";
+import IconsResolver from "unplugin-icons/resolver";
+import Icons from "unplugin-icons/vite";
+import Components from "unplugin-vue-components/vite";
+import VueRouter from "unplugin-vue-router/vite";
+import path from "path";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    VueRouter({}),
+    vue(),
+    Components({
+      dts: true,
+      resolvers: [IconsResolver()],
+    }),
+    Icons({
+      autoInstall: true,
+    }),
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+
+  server: {
+    host: "0.0.0.0", // 允许外部访问
+    port: 3000, // 指定端口
+    proxy: {
+      "/api": {
+        target: "http://100.111.86.103:9999",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
